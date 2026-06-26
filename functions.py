cac770511592eab9e3ab781234c6a52fd2a71c75
SystG <laurianegsys@gmail.com>
2026-06-25 22:49:53 -0400

    pratice return with function logic

--- functions.py
+++ functions.py
@@ -1,12 +1,31 @@
-def defined_age():
-    return 15
+def get_age():
+    age = int(input('What is your age?' ))
 
-age = defined_age()
+age = get_age()
 print(age)
 
-def double(number ):
-    return number*3
+def get_age():
+    return int(input('What is your age?' ))
+age = get_age()
+print(age)
 
-result = double (10)
-print(result)
+def get_number():
+    return int(input('What is your number?' ))
+number = get_number()
+print(number * 5)
+if number > 18:
+    print('is an adult or Gardian present?')
+else:
+    print('Welcome,you can proceed ')
+
+
+def get_temperature():
+    return int(input('What is the temperature?' ))
+
+def weather_message(temp):
+    if temp >= 30 :
+        print("It's not to hot today")
+
+temperature = get_temperature()
+weather_message(temperature)
 
