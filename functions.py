bb89ea219ec5a2bd7c331305d51ca8b0db5c9017
SystG <laurianegsys@gmail.com>
2026-06-30 17:19:43 -0400

    list practice

--- functions.py
+++ functions.py
@@ -1,31 +1,11 @@
-def get_age():
-    age = int(input('What is your age?' ))
-
-age = get_age()
-print(age)
-
-def get_age():
-    return int(input('What is your age?' ))
-age = get_age()
-print(age)
-
-def get_number():
-    return int(input('What is your number?' ))
-number = get_number()
-print(number * 5)
-if number > 18:
-    print('is an adult or Gardian present?')
-else:
-    print('Welcome,you can proceed ')
-
-
-def get_temperature():
-    return int(input('What is the temperature?' ))
-
-def weather_message(temp):
-    if temp >= 30 :
-        print("It's not to hot today")
-
-temperature = get_temperature()
-weather_message(temperature)
-
+foods = ['pizza', 'pate', 'eggs']
+cities =['paris', 'lagos', 'italy']
+languages = ['french', 'greek', 'spanish']
+print(foods)
+print(cities)
+print(languages)
+print(foods [0])
+print(cities [1])
+print(languages [2])
+student = ['Emelei', '36','True']
+print(student[1])
