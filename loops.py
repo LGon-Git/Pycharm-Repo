413bb6732cb46de802ff59fb84f3a4869ed1e568
SystG <laurianegsys@gmail.com>
2026-07-01 23:49:05 -0400

    Practice loops with len()

---
+++ loops.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'bird','fish']
+for animal in animals:
+    print('I like my pet',animal)
+animals = ['dog', 'cat', 'bird','fish','pig']
+for animal in animals:
+    print(animal,'has',len(animal),'letters')
