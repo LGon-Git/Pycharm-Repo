634e834c114e8ea4a4de8ca5743941ae663c86a7
SystG <laurianegsys@gmail.com>
2026-07-01 23:56:54 -0400

    List_methods practice

---
+++ list_methods.py
@@ -0,0 +1,14 @@
+things = ['a','b','c','d','e']
+print(things)
+things.append('f')
+things.remove('d')
+things.pop(0)
+print(things)
+
+animals = ['dog','cat','bird','dog']
+print(animals)
+animals.append('elephant')
+print(animals)
+animals.remove('cat')
+animals.pop(2)
+
